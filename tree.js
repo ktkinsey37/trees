379a876e7fd429aba342fb3059c62e725903c671
Kevin K <ktkinsey37@gmail.com>
2022-03-22 20:59:56 -0600

    submit

--- tree.js
+++ tree.js
@@ -15,12 +15,24 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    let totalVal;
-    for (let i=0;i<this.root.children;i++){
-      totalVal += this.val
-      if (this.children){
-        totalVal += this.sumValues
+    let totalVal = 0;
+    let visitStack = [this.root];
+    while (visitStack.length){
+      let current = visitStack.pop()
+
+      if (current === null){
+        return 0;
       }
+    
+        if (typeof current.val === "number"){
+          totalVal += current.val
+        }
+
+
+      for (let child of current.children){
+        visitStack.push(child)
+      }
+
     }
     return totalVal;
   }
@@ -28,14 +40,52 @@ class Tree {
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let numEvens = 0;
+    let visitStack = [this.root];
+    while (visitStack.length){
+      let current = visitStack.pop()
 
+      if (current === null){
+        return 0;
+      }
+    
+        if (typeof current.val === "number" && current.val % 2 === 0){
+          numEvens += 1
+        }
+
+
+      for (let child of current.children){
+        visitStack.push(child)
+      }
+
+    }
+    return numEvens;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let numGreater = 0;
+    let visitStack = [this.root];
+    while (visitStack.length){
+      let current = visitStack.pop()
 
+      if (current === null){
+        return 0;
+      }
+    
+        if (typeof current.val === "number" && current.val > lowerBound){
+          numGreater += 1
+        }
+
+
+      for (let child of current.children){
+        visitStack.push(child)
+      }
+
+    }
+    return numGreater;
   }
 }
 
